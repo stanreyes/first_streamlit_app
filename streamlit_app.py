029f834d217160fb589f1f616ce3503352d104ac
stanreyes <144967724+stanreyes@users.noreply.github.com>
2023-09-14 10:06:57 +0800

    Update streamlit_app.py
    Line 21: Display fruit instead of Index

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,6 +17,9 @@ import pandas
 # Import csv form pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 
+# Display Fruit instead of Index
+my_fruit_list = my_fruit_list.set_index('Fruit')
+
 # Let's put a pick list here so they can pick the fruit they want to include 
 streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 
