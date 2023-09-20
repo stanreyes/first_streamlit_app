2cc10857b82cc1ddfb37c856dc720a1bf6896ec5
stanreyes <144967724+stanreyes@users.noreply.github.com>
2023-09-20 13:56:30 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,6 +44,8 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
 
 
+# stops running codes unit here
+streamlit.stop()
 
 import snowflake.connector
 
