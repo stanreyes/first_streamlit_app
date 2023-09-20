37cbb9fe7ec7ba1350542546a2d60099332b5eab
stanreyes <144967724+stanreyes@users.noreply.github.com>
2023-09-20 14:01:52 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,54 +1,55 @@
 import streamlit
+import pandas
+import requests
+import snowflake.connector
+1from urllib.error import URLError
 
+
+# import streamlit ############################################################################################################################
 streamlit.title('My Parents Healthy Diner')
-
 streamlit.header('Breakfast Favorites')
 streamlit.text('🥣    Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗    Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔    Hard-Boiled Free-Ranged Egg')
 streamlit.text('🥑🍞  Avocado Toast')
-   
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 
 
-import pandas
-
+# import pandas ############################################################################################################################
 # Import csv form pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-
 # Display Fruit instead of Index
 my_fruit_list = my_fruit_list.set_index('Fruit')
-
 # Let's put a pick list here so they can pick the fruit they want to include 
 fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
-
 # Only display selected fruits
 fruits_to_show = my_fruit_list.loc[fruits_selected]
-
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
-
-
 
 streamlit.header("Fruityvice Fruit Advice!")
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
 streamlit.write('The user entered ', fruit_choice)
 
-import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 
+
+
+# import requests ############################################################################################################################
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 # write your own comment -what does the next line do? 
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # write your own comment - what does this do?
 streamlit.dataframe(fruityvice_normalized)
 
 
+
 # stops running codes unit here
 streamlit.stop()
 
-import snowflake.connector
 
+
+# import snowflake.connector ############################################################################################################################
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
