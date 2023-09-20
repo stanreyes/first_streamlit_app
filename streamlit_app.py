0bf2472847cc401642076ee9b60fde8e017e968e
stanreyes <144967724+stanreyes@users.noreply.github.com>
2023-09-20 14:02:13 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit
 import pandas
 import requests
 import snowflake.connector
-1from urllib.error import URLError
+from urllib.error import URLError
 
 
 # import streamlit ############################################################################################################################
